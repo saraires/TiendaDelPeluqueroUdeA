bcb201fefbd7d2e80822a3ee9ad739c098947fe1
Sarai Restrepo <66284121+saraires@users.noreply.github.com>
2024-09-14 16:38:33 -0500

    Upload Project

---
+++ src/tiendadelpeluqueroudea/TiendaDelPeluqueroUdeA.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiendadelpeluqueroudea;
+
+/**
+ *
+ * @author blandonm
+ * @author sarair
+ * @author penalosan
+ */
+public class TiendaDelPeluqueroUdeA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Menú
+        // Método para ejcutar comprar.
+        // Método para calular artículos.
+    }
+    
+}
